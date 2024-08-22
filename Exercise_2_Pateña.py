6725738ceb6829f56768d217a0d6d2bc4ccb0f41
ChristianDalePatena <qcdsjpatena@tip.edu.ph>
2024-08-22 12:11:48 +0800

    Add files via upload

---
+++ Exercise_2_Pateña.py
@@ -0,0 +1,18 @@
+
+input_grades=int(input("Enter your grades for validation: "))
+
+if input_grades >= 75:
+ print('passed!')
+  
+elif input_grades == 74:
+    print('Remedial')
+    
+elif input_grades < 74 and input_grades >=0 :
+    print('failed')
+
+else:
+    print('Your entered an invalid input!')
+
+
+      
+    
